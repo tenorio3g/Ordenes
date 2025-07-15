08a83b98d7bc7d90754cb3ea12c3f06279609b21
Fortunato Tenorio <tenorio.3g@hotmail.com>
2025-07-15 17:36:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,22 @@ app = Flask(__name__)
 def index():
     return render_template("formulario.html")
 
-@app.route("/pdf")
+@app.route("/pdf", methods=["POST"])
 def generar_pdf():
-    html = render_template("pdf_template.html")
+    orden = {
+        "area": request.form["area"],
+        "trabajo": request.form["trabajo"],
+        "equipo": request.form["equipo"],
+        "fecha": request.form["fecha"],
+        "supervisor": request.form["supervisor"]
+    }
+
+    html = render_template("pdf_template.html", orden=orden)
     pdf_io = BytesIO()
     HTML(string=html).write_pdf(pdf_io)
     pdf_io.seek(0)
-    return send_file(pdf_io, mimetype="application/pdf", as_attachment=True, download_name="orden_simple.pdf")
+    return send_file(pdf_io, mimetype="application/pdf", as_attachment=True, download_name="orden.pdf")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto automáticamente
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
