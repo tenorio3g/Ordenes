14865890640577a41cb5d2702df39f5bea1b88ad
Fortunato Tenorio <tenorio.3g@hotmail.com>
2025-07-15 16:54:17 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, render_template, request, send_file
 from weasyprint import HTML
 from io import BytesIO
 
@@ -6,41 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '''
-    <html>
-      <head><title>Generador de PDF</title></head>
-      <body style="font-family:Arial; text-align:center; margin-top:50px;">
-        <h1>Exportar Orden de Trabajo</h1>
-        <a href="/pdf" style="padding:10px 20px; background-color:green; color:white; text-decoration:none;">Exportar a PDF</a>
-      </body>
-    </html>
-    '''
+    return render_template("formulario.html")
 
 @app.route("/pdf")
 def generar_pdf():
-    html = """
-    <html>
-      <head><meta charset='utf-8'></head>
-      <body>
-        <h1 style="text-align:center;">Orden de Trabajo</h1>
-        <p>Esta es una prueba simple para exportar un PDF desde Flask y WeasyPrint.</p>
-        <p><b>Área:</b> Empaque</p>
-        <p><b>Trabajo:</b> Revisión de ductos</p>
-        <p><b>Supervisor:</b> Celeste Tirado</p>
-      </body>
-    </html>
-    """
-
+    html = render_template("template_pdf.html")
     pdf_io = BytesIO()
     HTML(string=html).write_pdf(pdf_io)
     pdf_io.seek(0)
-
-    return send_file(
-        pdf_io,
-        mimetype="application/pdf",
-        as_attachment=True,
-        download_name="orden_simple.pdf"
-    )
+    return send_file(pdf_io, mimetype="application/pdf", as_attachment=True, download_name="orden_simple.pdf")
 
 if __name__ == "__main__":
     app.run(debug=True)
